314cbad6a34accbea5a3a53ea62d80378cd56b4e
yizhaonight <zhaoyi19960415@gmail.com>
2021-11-12 15:46:26 +0800

    new

--- go.mod
+++ go.mod
@@ -1,5 +1,14 @@
-module v2
+module mqserver
 
 go 1.17
 
-require github.com/streadway/amqp v1.0.0
+replace kumact.com/gosdk v0.0.0 => ../gosdk
+
+replace iiwsm/sdk v0.0.0 => ../iiwsm/sdk
+
+replace iiws_da/sdk v0.0.0 => ../iiws_da/sdk
+
+require (
+	github.com/streadway/amqp v1.0.0
+	kumact.com/gosdk v0.0.0
+)
--- publisher.go
+++ publisher.go
@@ -1,10 +1,10 @@
-package publisher
+package mqserver
 
 import (
-	"log"
 	"strings"
 
 	"github.com/streadway/amqp"
+	"kumact.com/gosdk/xutils/logutil"
 )
 
 const (
@@ -28,7 +28,7 @@ func Publish(methods, path string, ctl interface{}) (err error) {
 		if method == METHOD_GET {
 			return
 		} else {
-			err = Queue(path, ctl)
+			err = Queue(path)
 			if err != nil {
 				return
 			}
@@ -40,43 +40,61 @@ func Publish(methods, path string, ctl interface{}) (err error) {
 /*
  * Put request into a message queue
  */
-func Queue(path string, ctl interface{}) (err error) {
-	// ip, err := GetIP() // Get the client ip to whom we publish our message
+func Queue(path string) (err error) {
+	ip, err := GetIP() // Get the client ip to whom we publish our message
 	if err != nil {
-		log.Fatal(err)
+		logutil.Error(err)
 		return
 	}
 	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
 	if err != nil {
-		log.Fatalf("%s: %s", "Cannot establish connection", err)
+		logutil.Error(err)
 		return
 	}
 	defer conn.Close()
 
 	ch, err := conn.Channel()
 	if err != nil {
-		log.Fatalf("%s: %s", "Cannot build channel", err)
+		logutil.Error(err)
 		return
 	}
 	defer ch.Close()
 
 	err = ch.ExchangeDeclare(
-		"mps_subscribe", // exchangeName
-		"topic",         // kind
-		true,            // durable
-		false,           // autoDelete
-		false,           // internal
-		false,           // noWait
-		nil,             // args
+		"mps_subscribe_demo", // exchangeName
+		"topic",              // kind
+		true,                 // durable
+		false,                // autoDelete
+		false,                // internal
+		false,                // noWait
+		nil,                  // args
 	)
 	if err != nil {
-		log.Fatalf("%s: %s", "Cannot declare exchange", err)
+		logutil.Error(err)
 		return
 	}
 
+	err = ch.Publish(
+		"mps_subscribe_demo", // exchange name
+		ip,                   // routing key
+		false,                // mandatory
+		false,                // immediate
+		amqp.Publishing{
+			DeliveryMode: amqp.Persistent,
+			ContentType:  "text/palin",
+			Body:         []byte(path),
+		},
+	)
+
+	logutil.Debug("[x] Sent " + path + " to " + ip)
 	return
 }
 
-func GetIP() (string, err error) {
-	return
+/*
+ * Get IP Address of edgeserver.
+ */
+func GetIP() (ip string, err error) {
+	// api to be completed
+	testIP := "1.1.1.1"
+	return testIP, err
 }
